c30ed799617a7a2362ca7599dd9aa2e11003e94c
Noe Trevino <noetrevino.music@gmail.com>
2024-09-15 00:19:00 -0500

    add: decryption script

---
+++ decrypt.py
@@ -0,0 +1,28 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+# TODO: make this also recursively decrypt like the encrypt file
+for file in os.listdir():
+    # avoid acciently locking out current progress
+    if file == "decrypt.py" or file == "encrypt.py" or file == "key.txt":
+        pass
+    else:
+        files.append(file)
+
+print(files)
+
+with open("key.txt", "rb") as key:
+    secretkey = key.read()
+
+for file in files:
+    with open(file, "rb") as _file:
+        contents = _file.read()
+    dec_content = Fernet(secretkey).decrypt(contents)
+    with open(file, "wb") as _file:
+        _file.write(dec_content)
+
+#     enc_content = Fernet(key).encrypt(contents)
+#     with open(file, "wb") as _file:
+#         _file.write(enc_content)
